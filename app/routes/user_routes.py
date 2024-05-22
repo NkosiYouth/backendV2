9223bfe933a1848af158bccfa918cb0c82d864d3
NkosiYouth <nkosi@sayouthatwork.com>
2024-05-22 20:37:47 +0200

    "Updated user routes: changed HTTP method for verify user route, added file upload and deletion logic, and reformatted comments."

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,6 +1,7 @@
 from flask import request, jsonify, Blueprint
 from app.models.User import User  # Import the User model
 from ..utils import upload_file_to_sharepoint, delete_file_or_folder
+
 user_bp = Blueprint('user', __name__)
 
 @user_bp.route('/users', methods=['POST'])
@@ -10,8 +11,8 @@ def create_user():
     user_id = user.save()
     return jsonify({"message": "User created successfully", "user_id": user_id}), 201
 
-# Existing route to update user by id
-@user_bp.route('/users/verify/<user_id>', methods=['PUT'])
+# Route to verify and update user by id
+@user_bp.route('/users/verify/<user_id>', methods=['POST'])
 def verify_user(user_id):
     data = request.json
     data['isValidated'] = True
@@ -19,13 +20,14 @@ def verify_user(user_id):
     print(user_id)
     result = User.update_user(user_id, data)
     if result:
-        upload_file_to_sharepoint(data['files'][0])
-        delete_file_or_folder(data['files'][0])
-        return jsonify({"message": "User updated successfully"}), 200
+        if 'files' in data and data['files']:
+            upload_file_to_sharepoint(data['files'][0])
+            delete_file_or_folder(data['files'][0])
+        return jsonify({"message": "User verified and updated successfully"}), 200
     else:
         return jsonify({"message": "User not found"}), 404
 
-# Existing route to update user by id
+# Route to update user by id
 @user_bp.route('/users/<user_id>', methods=['PUT'])
 def update_user(user_id):
     data = request.json
@@ -37,14 +39,14 @@ def update_user(user_id):
     else:
         return jsonify({"message": "User not found"}), 404
 
-# Existing route to get all users
+# Route to get all users
 @user_bp.route('/users', methods=['GET'])
 def get_all_users():
     query_params = request.args.to_dict()
     users = User.get_all_users(query_params)
     return jsonify(users)
 
-# Existing route to get a user by ID
+# Route to get a user by ID
 @user_bp.route('/users/<user_id>', methods=['GET'])
 def get_user(user_id):
     user = User.find_by_id(user_id)
