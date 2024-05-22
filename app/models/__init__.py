685844b2d4d738023536e5427bca3a768920d18d
NkosiYouth <nkosi@sayouthatwork.com>
2024-05-22 12:34:15 +0200

    revert

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -2,5 +2,3 @@ from .User import User
 from .Host import Host
 from .HostAddress import HostAddress
 from .Supervisor import Supervisor
-from .RawData import RawData
-from .FailedDocuments import FailedDocuments
