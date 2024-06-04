1d5099ebc8a6b68d50c3b298dcb9740a4ba2f822
NkosiYouth <nkosi@sayouthatwork.com>
2024-06-04 20:41:17 +0200

    awais updates

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,7 +1,6 @@
 from flask import request, jsonify, Blueprint
 from app.models.User import User  # Import the User model
 from ..utils import upload_file_to_sharepoint, delete_file_or_folder
-
 user_bp = Blueprint('user', __name__)
 
 @user_bp.route('/users', methods=['POST'])
@@ -11,23 +10,22 @@ def create_user():
     user_id = user.save()
     return jsonify({"message": "User created successfully", "user_id": user_id}), 201
 
-# Route to verify and update user by id
-@user_bp.route('/users/verify/<user_id>', methods=['POST'])
-def verify_user(user_id):
+# Existing route to update user by id
+@user_bp.route('/users/verify/<user_id>', methods=['PUT'])
+def update_user(user_id):
     data = request.json
     data['isValidated'] = True
     print(data)
     print(user_id)
     result = User.update_user(user_id, data)
     if result:
-        if 'files' in data and data['files']:
-            upload_file_to_sharepoint(data['files'][0])
-            delete_file_or_folder(data['files'][0])
-        return jsonify({"message": "User verified and updated successfully"}), 200
+        upload_file_to_sharepoint(data['files'][0])
+        delete_file_or_folder(data['files'][0])
+        return jsonify({"message": "User updated successfully"}), 200
     else:
         return jsonify({"message": "User not found"}), 404
 
-# Route to update user by id
+# Existing route to update user by id
 @user_bp.route('/users/<user_id>', methods=['PUT'])
 def update_user(user_id):
     data = request.json
@@ -39,14 +37,14 @@ def update_user(user_id):
     else:
         return jsonify({"message": "User not found"}), 404
 
-# Route to get all users
+# Existing route to get all users
 @user_bp.route('/users', methods=['GET'])
 def get_all_users():
     query_params = request.args.to_dict()
     users = User.get_all_users(query_params)
     return jsonify(users)
 
-# Route to get a user by ID
+# Existing route to get a user by ID
 @user_bp.route('/users/<user_id>', methods=['GET'])
 def get_user(user_id):
     user = User.find_by_id(user_id)
